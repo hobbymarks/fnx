0e8ee766d83878b9e1e03f91b8450be963e658d8
hobbymarks <ihobbymarks@gmail.com>
2021-06-08 10:25:13 +0800

    change python setup file

--- python-setup-twine-fdn.sh
+++ python-setup-twine-fdn.sh
@@ -19,6 +19,18 @@ if [[ $REPLY =~ ^[Yy][Ee][Ss]$ ]]; then
   twine upload --repository pypi dist/*
   printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' \#
   printf "upload finished.\n"
+fi
+
+printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+###############################################################################
+read -p "clean all build files in local? yes/[no] " -r
+printf '\n'
+if [[ $REPLY =~ ^[Yy][Ee][Ss]$ ]]; then
+  rm -fr dist
+  rm -fr build
+  rm -fr fdn.egg-info
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' \#
+  printf "clean finished.\n"
 else
   exit 1
 fi
