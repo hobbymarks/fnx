dfb40b029e23850ec159abd0df2c5a8873bf4de6
hobbymarks <ihobbymarks@gmail.com>
2022-09-07 12:10:33 +0800

    new version

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
-__version = "2022.5.5.3240"
+__version = "2022.6.26.2937"
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                        "README.rst"),
